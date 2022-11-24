87ef98081df88162db5933d382705d67a4840974
nguyenvangithub <81097647+nguyenvangithub@users.noreply.github.com>
2022-11-24 17:14:12 +0700

    heroku

---
+++ config/db/index.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+
+dotenv.config()
+
+const url = process.env.URL_DB;
+async function connect() {
+  try {
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connect successfully!!!!");
+  } catch (error) {
+    console.log(error);
+  }
+}
+module.exports = { connect };
+
+//"mongodb://localhost:27017/ChatAppLan1"
